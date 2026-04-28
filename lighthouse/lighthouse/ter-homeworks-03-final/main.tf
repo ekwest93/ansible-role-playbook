0d59ddbcc7fe102c47900ca66779ec339ee8a53b
ekwest93 <ekwest83@gmail.com>
2026-04-28 17:17:55 +0300

    Complete Lighthouse role: tasks, handlers, templates, defaults, meta

---
+++ lighthouse/lighthouse/ter-homeworks-03-final/main.tf
@@ -0,0 +1,47 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+resource "yandex_vpc_security_group" "example" {
+  name       = "example_dynamic"
+  network_id = yandex_vpc_network.develop.id
+  folder_id  = var.folder_id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "http"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "https"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "all out"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+locals {
+  ssh_key = file(var.public_key_path)
+}
